e04c9f0d876705b512491a150f3ac9bc35425f74
Bogdan <103338406+bogdanmb04@users.noreply.github.com>
2025-05-07 12:13:42 +0300

    Configured server to take images from camera

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from random import randint
+import numpy as np
+import requests
+from detectron2.data import MetadataCatalog
+from detectron2.data.datasets import register_coco_instances
+from detectron2.utils.visualizer import Visualizer
 from flask import Flask, request
 from PIL import Image
+import torch
 from model import get_saved_model, get_class_labels
+import cv2
+from io import BytesIO
 
 app = Flask(__name__)
 model = get_saved_model()
-
+register_coco_instances("trashnet_test", {'thing_classes': get_class_labels()}, "images/test/_annotations.coco.json", "images/test")
+test_metadata = MetadataCatalog.get("trashnet_test")
 
 @app.route('/')
 def home():
@@ -22,14 +31,31 @@ def hello():
     return 'Hello Arduino!'
 
 
-def get_image():
+def get_image() -> np.ndarray:
     # go to camera web server and grab image
-    ...
+    # static camera web server address: 192.168.74.12/cam-hi.jpg
+    esp_url: str = "http://192.168.74.12/cam-hi.jpg"
+    resp = requests.get(esp_url, timeout=5)
+    resp.raise_for_status()
+    image = Image.open(BytesIO(resp.content))
+    img_arr = np.asarray(image)
+    return img_arr
 
-
-@app.route("/class", methods=['POST'])
-def classify_image() -> int:
-    return 0
+@app.route("/class", methods=['GET'])
+def classify_image():
+    img: np.ndarray  = get_image()
+    out = model(img)
+    v = Visualizer(img[:,:,::-1], metadata=test_metadata)
+    v = v.draw_instance_predictions(out["instances"].to("cpu"))
+    cv2.imshow("image", v.get_image())
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    labels = get_class_labels()
+    if len(out['instances'].pred_classes) != 0:
+        class_idx = out['instances'].pred_classes[0]
+        return labels[class_idx]
+    else:
+        return "idk"
 
 
 
--- model.py
+++ model.py
@@ -1,17 +1,19 @@
+import torch
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
 from typing import List
 
 def get_saved_model() -> DefaultPredictor:
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
     cfg = get_cfg()
     cfg.merge_from_file("./output/configuration.yaml")
     cfg.MODEL.WEIGHTS = "output/model_final.pth"
-    cfg.MODEL.DEVICE = "cuda"
+    cfg.MODEL.DEVICE = device.type
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
     return DefaultPredictor(cfg)
 
 def get_class_labels() -> List[str]:
-    return ['idk', 'cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash'] #not queueing up the stupid instances just to get the class labels
-#legit just copy/pasted them from the notebook
-#nobody will know except whoever is reading this
-#why are you reading this?
+    return ['idk', 'cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
